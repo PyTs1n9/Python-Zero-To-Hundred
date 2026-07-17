b56af883abc41b509e7fc70517bcc01b9cf204a8
PyTs1n9 <1990629881@qq.com>
2026-07-17 15:49:59 +0800

    013 topic

---
+++ Topics/013 素数判断/013 main.py
@@ -0,0 +1,13 @@
+import math
+
+
+def is_prime(n):
+	if n < 2:
+		return False
+	for i in range(2,math.isqrt(n)+1):
+		if n % i ==0:
+			return False
+	return True
+
+if __name__ == '__main__':
+	print(is_prime(17))
