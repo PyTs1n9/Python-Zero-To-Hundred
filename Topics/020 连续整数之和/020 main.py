09c84ca3d2673a9b6785cc0824bb50e94411c1f7
PyTs1n9 <1990629881@qq.com>
2026-07-18 23:53:52 +0800

    020 topic

--- Topics/020 连续整数之和/020 main.py
+++ Topics/020 连续整数之和/020 main.py
@@ -1,19 +1,18 @@
-def consecutive_sum_solutions(n):
-    solutions = []
-    for start in range(1, n):
-        total = 0
-        current_solution = []
-        for number in range(start, n + 1):
-            total += number
-            current_solution.append(number)
-            if total == n:
-                if len(current_solution) >= 2:
-                    solutions.append(current_solution)
-                break
-            if total > n:
-                break
-    return solutions
-
+# def consecutive_sum_solutions(n):
+#     solutions = []
+#     for start in range(1, n):
+#         total = 0
+#         current_solution = []
+#         for number in range(start, n + 1):
+#             total += number
+#             current_solution.append(number)
+#             if total == n:
+#                 if len(current_solution) >= 2:
+#                     solutions.append(current_solution)
+#                 break
+#             if total > n:
+#                 break
+#     return solutions
 
 if __name__ == "__main__":
-    print(consecutive_sum_solutions(15))
+    # print(consecutive_sum_solutions(15))
